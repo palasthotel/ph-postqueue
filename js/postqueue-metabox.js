81edb87d02b3b1aaa2d45569bc5d4ae6f06d8aad
EdwardBock <edward.bock@palasthotel.de>
2017-12-15 13:07:40 +0100

    variable declaration

--- js/postqueue-metabox.js
+++ js/postqueue-metabox.js
@@ -3,80 +3,84 @@
  */
 (function( $, objectL10n ) {
 	'use strict';
+
+	const wrapperSelector = '.postqueue-metabox-wrapper';
+
 	/**
 	 * Start after dom is ready
 	 */
 	$(function() {
 
-		const $messages = $('.postqueue-metabox-wrapper').find('.messages');
+		const $messages = $(wrapperSelector).find('.messages');
 		postqueue_check_empty_list();
 
 		postqueue_add_remove_eventlisteners( $messages );
 
-    $('.postqueue-add').on( 'click', function(e) {
-      let $parent = $(this).closest('.postqueue-metabox-postqueueselect-wrapper');
-  		$parent.addClass('is-loading');
-      let postqueue_select_value = $parent.find('.postqueue-select').val();
+	    $('.postqueue-add').on( 'click', function(e) {
+	      const $parent = $(this).closest('.postqueue-metabox-postqueueselect-wrapper');
+	        $parent.addClass('is-loading');
+	      const postqueue_select_value = $parent.find('.postqueue-select').val();
 
-      if( postqueue_select_value !== 'none' ) {
-        let postid = $(this).attr('data-postid');
-        let queueid = postqueue_select_value;
-        let $selectedoption = $parent.find('[value="' + postqueue_select_value + '"]');
-        let queuename = $selectedoption.data('queuename');
-    		let data = {
-    			'action': 'postqueue_add_post',
-    			'postid': postid,
-    			'queueid': queueid
-    		};
+	      if( postqueue_select_value !== 'none' ) {
+	        const postid = $(this).attr('data-postid');
+	        const queueid = postqueue_select_value;
+	        const $selectedoption = $parent.find('[value="' + postqueue_select_value + '"]');
+	        const queuename = $selectedoption.data('queuename');
+	            const data = {
+	                'action': 'postqueue_add_post',
+	                'postid': postid,
+	                'queueid': queueid
+	            };
 
-    		jQuery.post( ajaxurl, data, function(response) {
-          if( response <= 0 ) {
-            $messages.text(objectL10n.erroroccured);
-      			$messages.addClass('error');
-    			} else {
-      			$messages.text(objectL10n.postadded);
-      			$messages.removeClass('error');
-      			postqueue_metabox_remove_selectoption( queueid, queuename, postid );
-      			postqueue_metabox_add_listitem( queueid, queuename, postid );
-      			postqueue_check_empty_list();
-    			}
-    			$parent.removeClass('is-loading');
-    		});
-      } else {
-        $messages.text(objectL10n.pleasechoose);
-        $parent.removeClass('is-loading');
-        $messages.addClass('error');
-      }
-    });
+	            $.post( ajaxurl, data, function(response) {
+	          if( response <= 0 ) {
+	            $messages.text(objectL10n.erroroccured);
+	                $messages.addClass('error');
+	                } else {
+	                $messages.text(objectL10n.postadded);
+	                $messages.removeClass('error');
+	                postqueue_metabox_remove_selectoption( queueid, queuename, postid );
+	                postqueue_metabox_add_listitem( queueid, queuename, postid );
+	                postqueue_check_empty_list();
+	                }
+	                $parent.removeClass('is-loading');
+	            });
+	      } else {
+	        $messages.text(objectL10n.pleasechoose);
+	        $parent.removeClass('is-loading');
+	        $messages.addClass('error');
+	      }
+	    });
 	});
 
 	/*
    * helper function, adds selectoption to metabox DOM
    */
 	function postqueue_metabox_add_selectoption( queueid, queuename, postid ) {
-  	let $wrapper = $('.postqueue-metabox-wrapper').find('.postqueue-metabox-postqueueselect-wrapper');
-  	$wrapper.find('select').append('<option value="'+queueid+'" data-queuename="'+queuename+'">'+queuename+'</option>');
+	    const $wrapper = $(wrapperSelector).find('.postqueue-metabox-postqueueselect-wrapper');
+	    $wrapper.find('select').append('<option value="'+queueid+'" data-queuename="'+queuename+'">'+queuename+'</option>');
 	}
 	/*
    * helper function, removes selectoption to metabox DOM
    */
 	function postqueue_metabox_remove_selectoption( queueid, queuename, postid ) {
-  	let $wrapper = $('.postqueue-metabox-wrapper').find('.postqueue-metabox-postqueueselect-wrapper');
-  	$wrapper.find("[value='" + queueid + "']").remove();
-  }
+	    const $wrapper = $(wrapperSelector).find('.postqueue-metabox-postqueueselect-wrapper');
+	    $wrapper.find("[value='" + queueid + "']").remove();
+	}
   /*
    * helper function, adds listitem to metabox DOM
    */
   function postqueue_metabox_add_listitem( queueid, queuename, postid ) {
-    let $wrapper = $('.postqueue-metabox-wrapper').find('.postqueue-metabox-postqueuelist-wrapper');
+
+    const $wrapper = $(wrapperSelector).find('.postqueue-metabox-postqueuelist-wrapper');
     $wrapper.find('ul').append('<li>'+queuename+'<span class="dashicons dashicons-no postqueue-remove" data-queueid="'+queueid+'" data-postid="'+postid+'" title="'+objectL10n.removepostfromthispostqueue+'" data-queuename="'+queuename+'"></span></li>');
-    postqueue_add_remove_eventlisteners( $('.postqueue-metabox-wrapper').find('.messages') );
+    postqueue_add_remove_eventlisteners( $(wrapperSelector).find('.messages') );
   }
   /*
    * helper function, removes listitem from metabox DOM
    */
   function postqueue_metabox_remove_listitem( queueid, queuename, postid ) {
-    let $wrapper = $('.postqueue-metabox-wrapper').find('.postqueue-metabox-postqueuelist-wrapper');
+    const $wrapper = $(wrapperSelector).find('.postqueue-metabox-postqueuelist-wrapper');
     $wrapper.find("[data-queueid='" + queueid + "']").closest('li').remove();
   }
 
@@ -84,7 +88,7 @@
    * helper function, checks if list is empty and prints a text if so
    */
   function postqueue_check_empty_list() {
-    let $wrapper = $('.postqueue-metabox-wrapper').find('.postqueue-metabox-postqueuelist-wrapper ul');
+    const $wrapper = $(wrapperSelector).find('.postqueue-metabox-postqueuelist-wrapper ul');
     if( !$wrapper.html().trim() ) {
       $wrapper.parent().append('<span class="postqueue-metabox-postqueuelist-emptyinfo">'+objectL10n.notstoredyet+'</span>');
     } else {
@@ -95,12 +99,12 @@
   function postqueue_add_remove_eventlisteners( $messages ) {
     $('.postqueue-remove').on( 'click', function(e) {
   		const $this = $(this);
-  		let $parent = $this.closest('.postqueue-metabox-postqueuelist-wrapper');
+  		const $parent = $this.closest('.postqueue-metabox-postqueuelist-wrapper');
   		$parent.addClass('is-loading');
-      let postid = $this.data('postid');
-      let queueid = $this.data('queueid');
-      let queuename = $this.data('queuename');
-  		let data = {
+        const postid = $this.data('postid');
+        const queueid = $this.data('queueid');
+        const queuename = $this.data('queuename');
+  		const data = {
   			'action': 'postqueue_remove_post',
   			'postid': postid,
   			'queueid': queueid
