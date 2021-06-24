d9e8b20e96605f6ddb9ccd0ab89475b340b83423
EdwardBock <edward.bock@palasthotel.de>
2021-06-24 08:25:20 +0200

    autoload update

--- public/vendor/composer/autoload_real.php
+++ public/vendor/composer/autoload_real.php
@@ -13,9 +13,6 @@ class ComposerAutoloaderInitee42ccb57c505152cc08f3d4df00bad8
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
@@ -23,12 +20,12 @@ class ComposerAutoloaderInitee42ccb57c505152cc08f3d4df00bad8
         }
 
         spl_autoload_register(array('ComposerAutoloaderInitee42ccb57c505152cc08f3d4df00bad8', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitee42ccb57c505152cc08f3d4df00bad8', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitee42ccb57c505152cc08f3d4df00bad8::getInitializer($loader));
         } else {
--- public/vendor/composer/autoload_static.php
+++ public/vendor/composer/autoload_static.php
@@ -20,16 +20,11 @@ class ComposerStaticInitee42ccb57c505152cc08f3d4df00bad8
         ),
     );
 
-    public static $classMap = array (
-        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInitee42ccb57c505152cc08f3d4df00bad8::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInitee42ccb57c505152cc08f3d4df00bad8::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInitee42ccb57c505152cc08f3d4df00bad8::$classMap;
 
         }, null, ClassLoader::class);
     }
